325f14d63c1e096158219a79e8c78bb0786e02bf
Nurhashimah <nurhashimah77@gmail.com>
2013-01-23 09:53:33 +0800

    try to commit fifth_app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FifthApp::Application.config.secret_token = '7d8625e5fcb34b61903e3fdf5bbad6402c3d724fd0b16c1293ed478cce9688cf11c71a1970f0307280dad31a1e40e0aa4584479517e4abc220e077006eb09bbd'
